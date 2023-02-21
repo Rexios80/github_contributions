6a1a7e0bbeb73d4508d9fdbdd158586fc2398bf2
Rexios <rexios@rexios.dev>
2023-02-21 18:32:22 -0500

    Initial commit

---
+++ bin/git_contributions.dart
@@ -0,0 +1,5 @@
+import 'package:git_contributions/git_contributions.dart' as git_contributions;
+
+void main(List<String> arguments) {
+  print('Hello world: ${git_contributions.calculate()}!');
+}
---
+++ test/git_contributions_test.dart
@@ -0,0 +1,8 @@
+import 'package:git_contributions/git_contributions.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
