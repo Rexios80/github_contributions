9f18f7ff64e8e4071e18bd1495a57aeceb55400d
Aaron DeLory <adelory@aptima.com>
2023-11-09 09:37:57 -0500

    Use date search filter

--- bin/github_contributions.dart
+++ bin/github_contributions.dart
@@ -1,30 +1,32 @@
 import 'dart:io';
 import 'package:github/github.dart';
+import 'package:intl/intl.dart';
 
 import 'private.dart';
 
 final github = GitHub(auth: Authentication.withToken(Private.token));
 
+final dateFormat = DateFormat('yyyy-MM-dd');
+
 // Past 6 months
-final after = DateTime.now().subtract(const Duration(days: 180));
-final before = DateTime.now();
+final after =
+    dateFormat.format(DateTime.now().subtract(const Duration(days: 180)));
+final before = dateFormat.format(DateTime.now());
+
+final tags = [
+  'author:${Private.author}',
+  'is:public',
+  'is:pr',
+  'created:$after..$before',
+  ...Private.repoExclude.map((e) => '-repo:$e'),
+  ...Private.orgExclude.map((e) => '-user:$e'),
+].join(' ');
 
 void main(List<String> arguments) async {
-  print('Excluding orgs: ${Private.orgExclude}\n');
-
-  final tags = [
-    'author:${Private.author}',
-    'is:public',
-    'is:pr',
-    ...Private.repoExclude.map((e) => '-repo:$e'),
-    ...Private.orgExclude.map((e) => '-user:$e'),
-  ];
+  print('Excluding orgs: ${Private.orgExclude}');
 
   final groupedPrs = <String, List<Issue>>{};
-  await for (final pr in github.search.issues(tags.join(' '), pages: 9999)) {
-    if (pr.createdAt!.isBefore(after) || pr.createdAt!.isAfter(before)) {
-      continue;
-    }
+  await for (final pr in github.search.issues(tags, pages: 9999)) {
     groupedPrs.update(
       slugFromUrl(pr.url),
       (e) => e..add(pr),
